2cb942bd8da0d3a24e44890556487e581384efad
flydubai747 <flydubai747roblox@gmail.com>
2026-01-17 16:39:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ class JoinButtonView(discord.ui.View):
 class SessionVoteView(discord.ui.View):
     def __init__(self, duration_mins, target, staff_member):
         super().__init__(timeout=None)
-        self.voters = set() # This stores user IDs
+        self.voters = set()
         self.hc_votes = set()
         self.nf_votes = set()
         self.duration_mins = duration_mins
@@ -79,7 +79,6 @@ class SessionVoteView(discord.ui.View):
             self.voters.remove(interaction.user.id)
         else: 
             self.voters.add(interaction.user.id)
-            
         button.label = f"({len(self.voters)}) Vote"
         if len(self.voters) >= self.target and not self.goal_notified:
             self.goal_notified = True
@@ -126,9 +125,23 @@ class MyBot(commands.Bot):
         super().__init__(command_prefix="!", intents=intents)
 
     async def setup_hook(self):
-        # We need to tell the bot to remember the View even if it restarts
         self.add_view(JoinButtonView())
         await self.tree.sync()
+
+    # --- WELCOME MESSAGE EVENT ---
+    async def on_member_join(self, member):
+        channel = self.get_channel(WELCOME_CHANNEL_ID)
+        if channel:
+            # Get the current member count of the server
+            member_count = len(member.guild.members)
+            embed = discord.Embed(
+                title="Welcome to Hillside Provincial Roleplay!",
+                description=f"Welcome {member.mention} to the server! You are member **#{member_count}**. Make sure to read the rules and enjoy your stay.",
+                color=16533327
+            )
+            embed.set_thumbnail(url=member.display_avatar.url)
+            embed.set_footer(text=f"Hillside RP | Member Count: {member_count}")
+            await channel.send(embed=embed)
 
     async def on_message(self, message):
         if message.author == self.user:
@@ -227,25 +240,10 @@ async def ssustart(interaction: discord.Interaction):
     channel = bot.get_channel(SESSION_CHANNEL_ID)
     old_id = load_msg_id()
     winning_aop = "NF & HPH402" 
-    voter_pings = ""
     
     if old_id:
         try:
             old_msg = await channel.fetch_message(old_id)
-            
-            # --- PING LOGIC ---
-            # We look for the SessionVoteView attached to the old message
-            # and get the IDs of everyone who clicked 'Vote'
-            for component in old_msg.components:
-                for child in component.children:
-                    # Check if the view is still active in the bot's memory
-                    # This works as long as the bot hasn't restarted
-                    pass
-            
-            # Since the voters are stored in the View instance, we'll try to get them
-            # if the bot hasn't restarted.
-            # If your bot restarts often, you'd need to save these to a JSON file.
-            
             hc_count, nf_count = 0, 0
             for item in old_msg.components[0].children:
                 if "Hillside City" in item.label:
@@ -255,12 +253,6 @@ async def ssustart(interaction: discord.Interaction):
             
             if hc_count > nf_count: 
                 winning_aop = "Hillside City"
-            
-            # Get the list of pings from the view (if found)
-            # To make this reliable, we find the message and use a helper
-            # But since views are ephemeral in memory, the simplest way is 
-            # to check the mentions if the button added them, but here we 
-            # will just use the role ping + the specific notification.
             
             await old_msg.delete()
         except: pass
@@ -283,7 +275,6 @@ async def ssustart(interaction: discord.Interaction):
         aop_embed = discord.Embed(color=16533327, description="### The area of play ingame is <:northwindfalls:1462090977291403409> [Northwind Falls](https://media.discordapp.net/attachments/1322319257131946034/1446923555743993926/hillside_nf_and_hph402_aop_map.png) and <:hph402:1455577343132307476> [Hillside Provincial Highway 402](https://cdn.discordapp.com/attachments/1453065520390607050/1453065558709764258/tiny_transparent.png)")
         aop_embed.set_image(url="https://media.discordapp.net/attachments/1322319257131946034/1446923555743993926/hillside_nf_and_hph402_aop_map.png")
 
-    # This sends the main start message with the Role Ping
     await channel.send(content=f"<@&{PING_ROLE_ID}> **The session is now starting!**", embed=main_embed, view=JoinButtonView())
     aop_msg = await channel.send(embed=aop_embed)
     save_msg_id(aop_msg.id) 
@@ -313,5 +304,3 @@ async def ssushutdown(interaction: discord.Interaction):
     await interaction.followup.send("Session ended!")
 
 bot.run(os.getenv('DISCORD_TOKEN'))
-
-
